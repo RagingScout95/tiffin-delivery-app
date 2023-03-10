e3b6991781517e286732bee6d00fd00292082843
RagingScout95 <prakhar99rajput@gmail.com>
2023-03-10 19:15:07 +0530

    updated login xml

--- app/src/main/java/com/example/tiffindeliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/tiffindeliveryapp/MainActivity.kt
@@ -8,9 +8,11 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        setContentView(R.layout.activity_main)
+//        // hide status bar
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main)
         setContentView(R.layout.login)
     }
 }
